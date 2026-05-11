39daf9b3ffee42d40814c99a8ee6780422319dad
Andrey Putilov <Andrey.Putilov@materialplus.io>
2026-05-10 23:11:30 -0700

    Minify assets!

--- src/report_html_make_js_plugin_code.py
+++ src/report_html_make_js_plugin_code.py
@@ -1,4 +1,7 @@
 
+from .minify_assets import minify_js, minify_css
+
+
 
 def sanitize_comment(s):
     return f'{s}'.replace('*/','*-/')
@@ -17,13 +20,22 @@ def make_syntax(plugin):
     description = plugin['description']
     js = plugin['js']
     css = plugin['css']
-    scripts = r'''
-<style>
+    def process_pipeins(scripts):
+        return scripts.replace(
+            '{{NAME}}', sanitize_comment(sanitize_identifier(name))
+        ).replace(
+            '{{DESCRIPTION}}', sanitize_comment(description)
+        ).replace(
+            '{{JS}}', js
+        ).replace(
+            '{{CSS}}', css
+        )
+    css_scripts = r'''
     /* {{NAME}} - css */
     /* {{DESCRIPTION}} */
 {{CSS}}
-</style>
-<script>
+'''
+    js_scripts = r'''
     /* {{NAME}} - js */
     /* {{DESCRIPTION}} */
 (function(){
@@ -67,15 +79,19 @@ def make_syntax(plugin):
         printErrors(e);
         throw e;
     }
-})()
+})();
+'''
+    desc_comment = process_pipeins(r'''
+    <!-- {{NAME}} - css & js -->
+    <!-- {{DESCRIPTION}} -->
+''')
+    scripts = f'''
+{desc_comment}
+<style>
+{minify_css(process_pipeins(css_scripts))}
+</style>
+<script>
+{minify_js(process_pipeins(js_scripts))}
 </script>
 '''
-    return scripts.replace(
-        '{{NAME}}', sanitize_comment(sanitize_identifier(name))
-    ).replace(
-        '{{DESCRIPTION}}', sanitize_comment(description)
-    ).replace(
-        '{{JS}}', js
-    ).replace(
-        '{{CSS}}', css
-    )
+    return scripts
--- src/report_html_template.py
+++ src/report_html_template.py
@@ -2,77 +2,98 @@
 from importlib import resources
 
 from .report_html_make_js_plugin_code import make_syntax as make_plugin_html
+from .minify_assets import minify_js, minify_css
 
 
 
 
 
-TEMPLATE_HTML_CSS_NORMALIZECSS = resources.files("src.templates").joinpath("normalize.css").read_text()
+TEMPLATE_HTML_CSS_NORMALIZECSS = resources.files("src.templates").joinpath("normalize.css").read_text('utf-8')
+# TEMPLATE_HTML_CSS_NORMALIZECSS = minify_css(TEMPLATE_HTML_CSS_NORMALIZECSS)
 
-TEMPLATE_HTML_STYLES = resources.files("src.templates").joinpath("common.css").read_text()
+TEMPLATE_HTML_STYLES = resources.files("src.templates").joinpath("common.css").read_text('utf-8')
+TEMPLATE_HTML_STYLES = minify_css(TEMPLATE_HTML_STYLES)
 
-TEMPLATE_HTML_STYLES_TABLE = resources.files("src.templates").joinpath("mdmreporttable.css").read_text()
+TEMPLATE_HTML_STYLES_TABLE = resources.files("src.templates").joinpath("mdmreporttable.css").read_text('utf-8')
+TEMPLATE_HTML_STYLES_TABLE = minify_css(TEMPLATE_HTML_STYLES_TABLE)
 
-TEMPLATE_HTML_COPYBANNER = resources.files("src.templates").joinpath("copybanner.html").read_text()
+TEMPLATE_HTML_COPYBANNER = resources.files("src.templates").joinpath("copybanner.html").read_text('utf-8')
 
-TEMPLATE_HTML_TABLE_BEGIN = resources.files("src.templates").joinpath("table_begin.html").read_text()
+TEMPLATE_HTML_TABLE_BEGIN = resources.files("src.templates").joinpath("table_begin.html").read_text('utf-8')
 
-TEMPLATE_HTML_TABLE_END = resources.files("src.templates").joinpath("table_end.html").read_text()
+TEMPLATE_HTML_TABLE_END = resources.files("src.templates").joinpath("table_end.html").read_text('utf-8')
 
-TEMPLATE_HTML_BEGIN = resources.files("src.templates").joinpath("html_begin.html").read_text()
+TEMPLATE_HTML_BEGIN = resources.files("src.templates").joinpath("html_begin.html").read_text('utf-8')
 
-TEMPLATE_HTML_END = resources.files("src.templates").joinpath("html_end.html").read_text()
+TEMPLATE_HTML_END = resources.files("src.templates").joinpath("html_end.html").read_text('utf-8')
 
 
 
 # plugins
-PLUGIN_BEAUTIFYDATES_JS = resources.files("src.templates.plugins.beautifydates").joinpath("script.js").read_text()
-PLUGIN_BEAUTIFYDATES_CSS = resources.files("src.templates.plugins.beautifydates").joinpath("script.css").read_text()
+PLUGIN_BEAUTIFYDATES_JS = resources.files("src.templates.plugins.beautifydates").joinpath("script.js").read_text('utf-8')
+PLUGIN_BEAUTIFYDATES_JS = minify_js(PLUGIN_BEAUTIFYDATES_JS)
+PLUGIN_BEAUTIFYDATES_CSS = resources.files("src.templates.plugins.beautifydates").joinpath("script.css").read_text('utf-8')
+PLUGIN_BEAUTIFYDATES_CSS = minify_css(PLUGIN_BEAUTIFYDATES_CSS)
 
 # plugins
-PLUGIN_ALIGNCOLWIDTHS_JS = resources.files("src.templates.plugins.aligncolwidths").joinpath("script.js").read_text()
-PLUGIN_ALIGNCOLWIDTHS_CSS = resources.files("src.templates.plugins.aligncolwidths").joinpath("script.css").read_text()
+PLUGIN_ALIGNCOLWIDTHS_JS = resources.files("src.templates.plugins.aligncolwidths").joinpath("script.js").read_text('utf-8')
+PLUGIN_ALIGNCOLWIDTHS_JS = minify_js(PLUGIN_ALIGNCOLWIDTHS_JS)
+PLUGIN_ALIGNCOLWIDTHS_CSS = resources.files("src.templates.plugins.aligncolwidths").joinpath("script.css").read_text('utf-8')
+PLUGIN_ALIGNCOLWIDTHS_CSS = minify_css(PLUGIN_ALIGNCOLWIDTHS_CSS)
 
 # plugins
-PLUGIN_EXPANDABLE_JS = resources.files("src.templates.plugins.expandable").joinpath("script.js").read_text()
-PLUGIN_EXPANDABLE_CSS = resources.files("src.templates.plugins.expandable").joinpath("script.css").read_text()
+PLUGIN_EXPANDABLE_JS = resources.files("src.templates.plugins.expandable").joinpath("script.js").read_text('utf-8')
+PLUGIN_EXPANDABLE_JS = minify_js(PLUGIN_EXPANDABLE_JS)
+PLUGIN_EXPANDABLE_CSS = resources.files("src.templates.plugins.expandable").joinpath("script.css").read_text('utf-8')
+PLUGIN_EXPANDABLE_CSS = minify_css(PLUGIN_EXPANDABLE_CSS)
 
 # plugins
-PLUGIN_SHOWHIDECOLUMNS_JS = resources.files("src.templates.plugins.showhidecolumns").joinpath("script.js").read_text()
-PLUGIN_SHOWHIDECOLUMNS_CSS = resources.files("src.templates.plugins.showhidecolumns").joinpath("script.css").read_text()
-
-# # plugins
-# PLUGIN_MEMORYSAVINGS_JS = resources.files("src.templates.plugins.memorysavings").joinpath("script.js").read_text()
-# PLUGIN_MEMORYSAVINGS_CSS = resources.files("src.templates.plugins.memorysavings").joinpath("script.css").read_text()
+PLUGIN_SHOWHIDECOLUMNS_JS = resources.files("src.templates.plugins.showhidecolumns").joinpath("script.js").read_text('utf-8')
+PLUGIN_SHOWHIDECOLUMNS_JS = minify_js(PLUGIN_SHOWHIDECOLUMNS_JS)
+PLUGIN_SHOWHIDECOLUMNS_CSS = resources.files("src.templates.plugins.showhidecolumns").joinpath("script.css").read_text('utf-8')
+PLUGIN_SHOWHIDECOLUMNS_CSS = minify_css(PLUGIN_SHOWHIDECOLUMNS_CSS)
 
 # plugins
-PLUGIN_JIRA_JS = resources.files("src.templates.plugins.jira").joinpath("script.js").read_text()
-PLUGIN_JIRA_CSS = resources.files("src.templates.plugins.jira").joinpath("script.css").read_text()
+PLUGIN_JIRA_JS = resources.files("src.templates.plugins.jira").joinpath("script.js").read_text('utf-8')
+PLUGIN_JIRA_JS = minify_js(PLUGIN_JIRA_JS)
+PLUGIN_JIRA_CSS = resources.files("src.templates.plugins.jira").joinpath("script.css").read_text('utf-8')
+PLUGIN_JIRA_CSS = minify_css(PLUGIN_JIRA_CSS)
 
 # plugins
-PLUGIN_COLUMNFILTERING_JS = resources.files("src.templates.plugins.columnfiltering").joinpath("script.js").read_text()
-PLUGIN_COLUMNFILTERING_CSS = resources.files("src.templates.plugins.columnfiltering").joinpath("script.css").read_text()
+PLUGIN_COLUMNFILTERING_JS = resources.files("src.templates.plugins.columnfiltering").joinpath("script.js").read_text('utf-8')
+PLUGIN_COLUMNFILTERING_JS = minify_js(PLUGIN_COLUMNFILTERING_JS)
+PLUGIN_COLUMNFILTERING_CSS = resources.files("src.templates.plugins.columnfiltering").joinpath("script.css").read_text('utf-8')
+PLUGIN_COLUMNFILTERING_CSS = minify_css(PLUGIN_COLUMNFILTERING_CSS)
 
 # plugins
-PLUGIN_SHOWHIDESECTIONS_JS = resources.files("src.templates.plugins.showhidesections").joinpath("script.js").read_text()
-PLUGIN_SHOWHIDESECTIONS_CSS = resources.files("src.templates.plugins.showhidesections").joinpath("script.css").read_text()
+PLUGIN_SHOWHIDESECTIONS_JS = resources.files("src.templates.plugins.showhidesections").joinpath("script.js").read_text('utf-8')
+PLUGIN_SHOWHIDESECTIONS_JS = minify_js(PLUGIN_SHOWHIDESECTIONS_JS)
+PLUGIN_SHOWHIDESECTIONS_CSS = resources.files("src.templates.plugins.showhidesections").joinpath("script.css").read_text('utf-8')
+PLUGIN_SHOWHIDESECTIONS_CSS = minify_css(PLUGIN_SHOWHIDESECTIONS_CSS)
 
 # plugins
-PLUGIN_FORMATPREOUTPUTS_JS = resources.files("src.templates.plugins.format_pre_outputs").joinpath("script.js").read_text()
-PLUGIN_FORMATPREOUTPUTS_CSS = resources.files("src.templates.plugins.format_pre_outputs").joinpath("script.css").read_text()
+PLUGIN_FORMATPREOUTPUTS_JS = resources.files("src.templates.plugins.format_pre_outputs").joinpath("script.js").read_text('utf-8')
+PLUGIN_FORMATPREOUTPUTS_JS = minify_js(PLUGIN_FORMATPREOUTPUTS_JS)
+PLUGIN_FORMATPREOUTPUTS_CSS = resources.files("src.templates.plugins.format_pre_outputs").joinpath("script.css").read_text('utf-8')
+PLUGIN_FORMATPREOUTPUTS_CSS = minify_css(PLUGIN_FORMATPREOUTPUTS_CSS)
 
 # plugins
-PLUGIN_VALIDATEMDDLABELISSUES_JS = resources.files("src.templates.plugins.validatemddlabelissues").joinpath("script.js").read_text()
-PLUGIN_VALIDATEMDDLABELISSUES_CSS = resources.files("src.templates.plugins.validatemddlabelissues").joinpath("script.css").read_text()
+PLUGIN_VALIDATEMDDLABELISSUES_JS = resources.files("src.templates.plugins.validatemddlabelissues").joinpath("script.js").read_text('utf-8')
+PLUGIN_VALIDATEMDDLABELISSUES_JS = minify_js(PLUGIN_VALIDATEMDDLABELISSUES_JS)
+PLUGIN_VALIDATEMDDLABELISSUES_CSS = resources.files("src.templates.plugins.validatemddlabelissues").joinpath("script.css").read_text('utf-8')
+PLUGIN_VALIDATEMDDLABELISSUES_CSS = minify_css(PLUGIN_VALIDATEMDDLABELISSUES_CSS)
 
 # plugins
-PLUGIN_DIFFONDIFFSHOWALLROWS_JS = resources.files("src.templates.plugins.diffondiffshowallrows").joinpath("script.js").read_text()
-PLUGIN_DIFFONDIFFSHOWALLROWS_CSS = resources.files("src.templates.plugins.diffondiffshowallrows").joinpath("script.css").read_text()
+PLUGIN_DIFFONDIFFSHOWALLROWS_JS = resources.files("src.templates.plugins.diffondiffshowallrows").joinpath("script.js").read_text('utf-8')
+PLUGIN_DIFFONDIFFSHOWALLROWS_JS = minify_js(PLUGIN_DIFFONDIFFSHOWALLROWS_JS)
+PLUGIN_DIFFONDIFFSHOWALLROWS_CSS = resources.files("src.templates.plugins.diffondiffshowallrows").joinpath("script.css").read_text('utf-8')
+PLUGIN_DIFFONDIFFSHOWALLROWS_CSS = minify_css(PLUGIN_DIFFONDIFFSHOWALLROWS_CSS)
 
 
 
 # plugin common code (launcher)
-PLUGINS_COMMON_JS = resources.files("src.templates").joinpath("common.js").read_text()
+PLUGINS_COMMON_JS = resources.files("src.templates").joinpath("common.js").read_text('utf-8')
+PLUGINS_COMMON_JS = minify_js(PLUGINS_COMMON_JS)
 
 
 
@@ -102,12 +123,6 @@ plugins = [
         'css': PLUGIN_SHOWHIDECOLUMNS_CSS,
         'js': PLUGIN_SHOWHIDECOLUMNS_JS,
     },
-    # {
-    #     'name': 'memorysavings',
-    #     'description': 'Auto-click on hidden sections to reveal; sections were previously hidden to reduce piece loaded into memory',
-    #     'css': PLUGIN_MEMORYSAVINGS_CSS,
-    #     'js': PLUGIN_MEMORYSAVINGS_JS,
-    # },
     {
         'name': 'jira',
         'description': 'Jira - related ticket lookup',
@@ -154,7 +169,8 @@ plugins = [
 ]
 
 
-TEMPLATE_HTML_SCRIPTS = ''.join([make_plugin_html(plugin) for plugin in plugins
+TEMPLATE_HTML_SCRIPTS = ''.join([
+    make_plugin_html(plugin) for plugin in plugins
 ])
 
 
